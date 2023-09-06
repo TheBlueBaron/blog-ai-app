7b5b586d8a46265b123c22c9933097905e68259c
TheBlueBaron <ronniehughjames@protonmail.com>
2023-09-06 21:45:20 +0100

    feat(): server component setup and card data population

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,3 +1,4 @@
+import { Post } from "@prisma/client";
 import Link from "next/link";
 import React from "react";
 
@@ -6,6 +7,7 @@ type Props = {
   imageHeight: string;
   isSmallCard?: boolean;
   isLongForm?: boolean;
+  post: Post;
 };
 
 const Card = ({
@@ -13,34 +15,47 @@ const Card = ({
   imageHeight,
   isSmallCard = false,
   isLongForm = false,
+  post,
 }: Props) => {
+  const { id, title, author, createdAt, image, snippet } = post || {};
+  const date = new Date(createdAt);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  } as Intl.DateTimeFormatOptions;
+  const formattedDate = date.toLocaleDateString("en-GB", options);
+
   return (
     <div className={className}>
-      <Link className="basis-full hover:opacity-70" href={"/"}>
+      <Link
+        className="basis-full hover:opacity-70"
+        href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+      >
         <div className={`relative w-auto mb-3 ${imageHeight}`}>Image</div>
       </Link>
       <div className="basis-full">
-        <Link href={"/"}>
+        <Link href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}>
           <h4
             className={`font-bold hover:text-accent-green
             ${isSmallCard ? "text-base" : "text-lg"}
             ${isSmallCard ? "line-clamp-2" : ""}
           `}
           >
-            Title
+            {title}
           </h4>
         </Link>
 
         <div className={`${isSmallCard ? "my-2" : "flex my-3"} gap-3`}>
-          <h5 className="font-semibold text-xs">Author</h5>
-          <h6 className="text-wh-300 text-xs">Date</h6>
+          <h5 className="font-semibold text-xs">{author}</h5>
+          <h6 className="text-wh-300 text-xs">{formattedDate}</h6>
         </div>
         <p
           className={`text-wh-100 ${
             isLongForm ? "line-clamp-5" : "line-clamp-3"
           }`}
         >
-          Snippet
+          {snippet}
         </p>
       </div>
     </div>
